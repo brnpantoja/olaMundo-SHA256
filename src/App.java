041772435c99bfc1687f447bc3a1ae2e40d2fa10
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-09-16 17:05:40 -0300

    Update

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-String input = "Olá Mundo";
+        String input = "Olá Mundo";
         String hash = SHA256Helper.getSHA256(input);
         System.out.println(hash);
     }
